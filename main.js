b565a95c116f55843f8f4390ffbbd06439cba87b
devjangid15 <jangiddev195@gmail.com>
2024-05-13 19:42:07 +0530

    FINAL CODE

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ window.addEventListener('load', () => {
 	const input = document.querySelector("#new-task-input");
 	const list_el = document.querySelector("#tasks");
 
+    
 	form.addEventListener('submit', (e) => {
 		e.preventDefault();
 
